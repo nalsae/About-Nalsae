6148b7df0d95601a37a182646f173cde80f32460
nalsae <nalsae0207@naver.com>
2023-09-29 22:40:59 +0900

    ✨ Feat : Home 페이지 임시 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,52 @@
+'use client';
+
+import * as THREE from 'three';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+
+import useStore from '@/stores/store';
+
+import Computer from '@/components/Computer';
+import Window from '@/components/Window';
+
 export default function Home() {
-  return <main>메인 페이지</main>;
+  const { isClick, isOn } = useStore();
+
+  return (
+    <main className="relative w-screen h-screen bg-animation">
+      <Canvas shadows camera={{ position: [-4, 2, 8] }}>
+        <ambientLight
+          color={new THREE.Color('#f3e7ff')}
+          position={[2, 2, 2]}
+          intensity={2}
+        />
+        <directionalLight
+          color={new THREE.Color('#ffffff')}
+          position={[2, 2, 2]}
+          intensity={0.1}
+        />
+        <directionalLight
+          color={new THREE.Color('#ffffff')}
+          position={[2, -1, -2]}
+          castShadow
+          intensity={1.2}
+        />
+        <pointLight
+          color={new THREE.Color('#e4cdff')}
+          position={[0, -0.8, 2.5]}
+          intensity={0.5}
+          decay={2}
+        />
+        <Computer />
+        <OrbitControls
+          enableRotate={!isClick}
+          enableZoom={false}
+          minPolarAngle={1.15}
+          maxPolarAngle={1.75}
+        />
+        {/* <gridHelper /> */}
+      </Canvas>
+      {isOn && <Window />}
+    </main>
+  );
 }
