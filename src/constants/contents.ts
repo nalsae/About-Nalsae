8cba018eff152764119d929445e0a93164abc290
nalsae <nalsae0207@naver.com>
2023-10-03 03:48:49 +0900

    ✨ Feat : 컴포넌트 구현에 필요한 상수 추가

--- src/constants/contents.ts
+++ src/constants/contents.ts
@@ -1,4 +1,4 @@
-export const ICONS_INFO = [
+export const SCREENS_INFO = [
   'About Me',
   '',
   '',
@@ -24,3 +24,40 @@ export const ICONS_INFO = [
   '',
   'Album',
 ];
+
+export const ABOUT_ME_CONTENTS = [
+  'Intro',
+  'Contact',
+  'Activity',
+  'Education',
+  'Interest',
+];
+
+export const PROJECTS_CONTENTS = [
+  'Grow Story',
+  'My Pokedex',
+  '33 1/3',
+  'Piece Plan',
+  'Improving BR Website',
+];
+
+export const TECH_STACKS_CONTENTS = ['Front-End', 'Back-End', 'Design'];
+
+export const ICONS_URL = {
+  'About Me': "bg-[url('/assets/icon/folder_lightpink.png')]",
+  Hobby: "bg-[url('/assets/icon/heart_purple.png')]",
+  Playlist: "bg-[url('/assets/icon/heart_pink.png')]",
+  Projects: "bg-[url('/assets/icon/folder_pink.png')]",
+  'Teck Stacks': "bg-[url('/assets/icon/folder_mint.png')]",
+  Album: "bg-[url('/assets/icon/note_mint.png')]",
+  Intro: "bg-[url('/assets/icon/note_mint.png')]",
+  Contact: "bg-[url('/assets/icon/note_white.png')]",
+  Activity: "bg-[url('/assets/icon/note_mint.png')]",
+  Education: "bg-[url('/assets/icon/note_white.png')]",
+  Interest: "bg-[url('/assets/icon/heart_purple.png')]",
+  'Grow Story': "bg-[url('/assets/icon/paper_growstory.png')]",
+  'My Pokedex': "bg-[url('/assets/icon/paper_pokedex.png')]",
+  '33 1/3': "bg-[url('/assets/icon/paper_33.png')]",
+  'Piece Plan': "bg-[url('/assets/icon/paper_pieceplan.png')]",
+  'Improving BR Website': "bg-[url('/assets/icon/paper_br.png')]",
+} as { [key: string]: string };
