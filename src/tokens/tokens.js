784ba7e87e615ac93828f02ccf741ae753859cdd
nalsae <nalsae0207@naver.com>
2023-10-02 23:18:34 +0900

    🔧 Conf : 디자인 토큰 파일 추가

---
+++ src/tokens/tokens.js
@@ -0,0 +1,52 @@
+const tokens = {
+  colors: {
+    blue: {
+      10: '#70cdff',
+      30: '#4e8ff9',
+      50: '#3663ad',
+      70: '#233f6f',
+      90: '#162947',
+    },
+    yellow: {
+      10: '#ffff51',
+      30: '#fff406',
+      50: '#ffcb05',
+      70: '#cca204',
+      90: '#a38203',
+    },
+    red: {
+      10: '#ff8172',
+      30: '#ff5949',
+      50: '#f73526',
+      70: '#c62a1e',
+      90: '#9e2218',
+    },
+    green: {
+      10: '#75b798',
+      30: '#479f76',
+      50: '#198754',
+      70: '#146c43',
+      90: '#0f5132',
+    },
+    purple: {
+      10: '#e2d3ff',
+      30: '#866bba',
+      50: '#513e78',
+      70: '#2f214d',
+    },
+    white: { 10: '#fefefe', 30: '#f9fafb', 50: '#f8f9fa' },
+    gray: { 10: '#c6c7c8', 30: '#959596', 50: '#7a7c7f', 70: '#4d5154' },
+    black: { 10: '#212529', 30: '#1a1e21', 50: '#141619' },
+  },
+  boxShadow: {
+    taskbar: '0px -4px 4px 0px rgba(81, 62, 120, 0.25);',
+    window: '4px 4px 4px 0px #6A43B8',
+    page: '2px 2px 0px 0px #513E78',
+  },
+  dropShadow: {
+    'icon-big': '2px 4px 2px #4C2994',
+    'icon-small': '3px 3px 2px #4C2994',
+  },
+};
+
+export default tokens;
