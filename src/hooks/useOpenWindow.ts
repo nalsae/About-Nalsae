48de9a5d23032260508eac9d975e19f8e8dd24ff
nalsae <nalsae0207@naver.com>
2023-10-03 16:41:11 +0900

    ✨ Feat : 커스텀 훅 useOpenWindow 추가 기능 구현

--- src/hooks/useOpenWindow.ts
+++ src/hooks/useOpenWindow.ts
@@ -1,19 +1,23 @@
 import useWindowStore from '@/stores/windowStore';
 
 const useOpenWindow = (type: 'folder' | 'page') => {
-  const { windows, open } = useWindowStore();
+  const { windows, open, restore } = useWindowStore();
 
   const handleDoubleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     if (event.target instanceof HTMLButtonElement) {
+      const name = event.target.dataset.windowName as string;
+      const url = event.target.dataset.url as string;
+
       const newWindow = {
         type,
-        name: event.target.dataset.windowName as string,
-        url: event.target.dataset.url as string,
+        name,
+        url,
         isMinimized: false,
         isMaximized: false,
       };
 
       open(newWindow);
+      restore(name);
     }
   };
 
