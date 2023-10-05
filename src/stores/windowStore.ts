c99a3b61efcb326632adabd12bbd02e0ed7bc99d
nalsae <nalsae0207@naver.com>
2023-10-06 04:32:21 +0900

    ✨ Feat : windowStore 내부 로직 추가 구현

--- src/stores/windowStore.ts
+++ src/stores/windowStore.ts
@@ -4,51 +4,366 @@ import uniqBy from 'lodash/uniqBy';
 
 type Window = {
   type: 'folder' | 'page';
+  title: string;
   url: string;
-  name: string;
+  isOpen: boolean;
   isMinimized: boolean;
   isMaximized: boolean;
 };
 
-export interface WindowState {
-  windows: Window[];
+export type Titles =
+  | 'About Me'
+  | 'Projects'
+  | 'Tech Stacks'
+  | 'Hobby'
+  | 'Playlist'
+  | 'Album'
+  | 'Email'
+  | 'Intro'
+  | 'Contact'
+  | 'Activity'
+  | 'Education'
+  | 'Interest'
+  | 'Grow Story'
+  | 'My Pokedex'
+  | '33 1/3'
+  | 'Piece Plan'
+  | 'Improving BR Website'
+  | 'Front-End'
+  | 'Back-End'
+  | 'Design'
+  | 'Austria_1'
+  | 'Austria_2'
+  | 'Croatia_1'
+  | 'Croatia_2'
+  | 'France_1'
+  | 'France_2'
+  | 'Portugal_1'
+  | 'Portugal_2'
+  | 'Switzerland_1'
+  | 'Switzerland_2';
 
-  open: (window: Window) => void;
-  minimize: (name: string) => void;
-  maximize: (window: Window) => void;
-  restore: (name: string) => void;
-  close: (name: string) => void;
+export interface WindowState {
+  tasks: Window[];
+
+  'About Me': Window;
+  Projects: Window;
+  'Tech Stacks': Window;
+  Hobby: Window;
+  Playlist: Window;
+  Album: Window;
+  Email: Window;
+
+  Intro: Window;
+  Contact: Window;
+  Activity: Window;
+  Education: Window;
+  Interest: Window;
+
+  'Grow Story': Window;
+  'My Pokedex': Window;
+  '33 1/3': Window;
+  'Piece Plan': Window;
+  'Improving BR Website': Window;
+
+  'Front-End': Window;
+  'Back-End': Window;
+  Design: Window;
+
+  Austria_1: Window;
+  Austria_2: Window;
+  Croatia_1: Window;
+  Croatia_2: Window;
+  France_1: Window;
+  France_2: Window;
+  Portugal_1: Window;
+  Portugal_2: Window;
+  Switzerland_1: Window;
+  Switzerland_2: Window;
+
+  addTask: (title: Titles) => void;
+  deleteTask: (title: Titles) => void;
+
+  open: (title: Titles) => void;
+  minimize: (title: Titles) => void;
+  toggleSize: (title: Titles) => void;
+  restore: (title: Titles) => void;
+  close: (title: Titles) => void;
 }
 
 const useWindowStore = create<WindowState>((set) => ({
-  windows: [],
+  tasks: [],
 
-  open: (window) =>
-    set((state) => ({ windows: uniqBy([...state.windows, window], 'name') })),
-  minimize: (name) =>
+  'About Me': {
+    type: 'folder',
+    title: 'About Me',
+    url: "bg-[url('/assets/icon/folder_lightpink.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  Projects: {
+    type: 'folder',
+    title: 'Projects',
+    url: "bg-[url('/assets/icon/folder_pink.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  'Tech Stacks': {
+    type: 'folder',
+    title: 'Tech Stacks',
+    url: "bg-[url('/assets/icon/folder_mint.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  Hobby: {
+    type: 'page',
+    title: 'Hobby',
+    url: "bg-[url('/assets/icon/heart_purple.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  Playlist: {
+    type: 'page',
+    title: 'Playlist',
+    url: "bg-[url('/assets/icon/heart_pink.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  Album: {
+    type: 'folder',
+    title: 'Album',
+    url: "bg-[url('/assets/icon/note_mint.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  Email: {
+    type: 'page',
+    title: 'Email',
+    url: '',
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+
+  Intro: {
+    type: 'page',
+    title: 'Intro',
+    url: "bg-[url('/assets/icon/note_mint.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  Contact: {
+    type: 'page',
+    title: 'Contact',
+    url: "bg-[url('/assets/icon/note_white.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  Activity: {
+    type: 'page',
+    title: 'Activity',
+    url: "bg-[url('/assets/icon/note_mint.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  Education: {
+    type: 'page',
+    title: 'Education',
+    url: "bg-[url('/assets/icon/note_white.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  Interest: {
+    type: 'page',
+    title: 'Interest',
+    url: "bg-[url('/assets/icon/heart_purple.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+
+  'Grow Story': {
+    type: 'page',
+    title: 'Grow Story',
+    url: "bg-[url('/assets/icon/paper_growstory.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  'My Pokedex': {
+    type: 'page',
+    title: 'My Pokedex',
+    url: "bg-[url('/assets/icon/paper_pokedex.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  '33 1/3': {
+    type: 'page',
+    title: '33 1/3',
+    url: "bg-[url('/assets/icon/paper_33.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  'Piece Plan': {
+    type: 'page',
+    title: 'Piece Plan',
+    url: "bg-[url('/assets/icon/paper_pieceplan.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  'Improving BR Website': {
+    type: 'page',
+    title: 'Improving BR Website',
+    url: "bg-[url('/assets/icon/paper_br.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+
+  'Front-End': {
+    type: 'page',
+    title: 'Front-End',
+    url: "bg-[url('/assets/icon/paper_pink.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  'Back-End': {
+    type: 'page',
+    title: 'Back-End',
+    url: "bg-[url('/assets/icon/paper_white.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  Design: {
+    type: 'page',
+    title: 'Design',
+    url: "bg-[url('/assets/icon/paper_pink.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+
+  Austria_1: {
+    type: 'page',
+    title: 'Austria_1',
+    url: "bg-[url('/assets/icon/photo_pink.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  Austria_2: {
+    type: 'page',
+    title: 'Austria_2',
+    url: "bg-[url('/assets/icon/photo_white.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  Croatia_1: {
+    type: 'page',
+    title: 'France_2',
+    url: "bg-[url('/assets/icon/photo_pink.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  Croatia_2: {
+    type: 'page',
+    title: 'France_2',
+    url: "bg-[url('/assets/icon/photo_white.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  France_1: {
+    type: 'page',
+    title: 'France_1',
+    url: "bg-[url('/assets/icon/photo_pink.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  France_2: {
+    type: 'page',
+    title: 'France_2',
+    url: "bg-[url('/assets/icon/photo_white.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  Portugal_1: {
+    type: 'page',
+    title: 'Portugal_1',
+    url: "bg-[url('/assets/icon/photo_pink.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  Portugal_2: {
+    type: 'page',
+    title: 'Portugal_2',
+    url: "bg-[url('/assets/icon/photo_white.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  Switzerland_1: {
+    type: 'page',
+    title: 'Switzerland_1',
+    url: "bg-[url('/assets/icon/photo_pink.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+  Switzerland_2: {
+    type: 'page',
+    title: 'Switzerland_2',
+    url: "bg-[url('/assets/icon/photo_white.png')]",
+    isOpen: false,
+    isMinimized: false,
+    isMaximized: false,
+  },
+
+  addTask: (title) =>
     set((state) => ({
-      windows: state.windows.map((window) =>
-        name === window.name ? { ...window, isMinimized: true } : window,
-      ),
+      tasks: uniqBy([...state.tasks, state[title]], 'title'),
     })),
-  maximize: (window) =>
+  deleteTask: (title) =>
     set((state) => ({
-      windows: state.windows.map(({ name }) =>
-        name === window.name ? { ...window, isMaximized: true } : window,
-      ),
+      tasks: state.tasks.filter((task) => task.title !== title),
     })),
-  restore: (name) =>
+
+  open: (title) =>
     set((state) => ({
-      windows: state.windows.map((window) =>
-        name === window.name
-          ? { ...window, isMinimized: false, isMaximized: false }
-          : window,
-      ),
+      [title]: { ...state[title], isOpen: true },
     })),
-  close: (name) =>
+  minimize: (title) =>
+    set((state) => ({ [title]: { ...state[title], isMinimized: true } })),
+  toggleSize: (title) =>
     set((state) => ({
-      windows: state.windows.filter((window) => name !== window.name),
+      [title]: { ...state[title], isMaximized: !state[title].isMaximized },
     })),
+  restore: (title) =>
+    set((state) => ({
+      [title]: { ...state[title], isMinimized: false, isMaximized: false },
+    })),
+  close: (title) =>
+    set((state) => ({ [title]: { ...state[title], isOpen: false } })),
 }));
 
 export default useWindowStore;
