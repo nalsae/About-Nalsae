2d617eff72f6f45ebd7c06ef4537692d430e98d2
nalsae <nalsae0207@naver.com>
2023-10-06 04:39:41 +0900

    ✨ Feat : Contact 컴포넌트에 추가 기능 구현

--- src/components/Page/AboutMe/Contact.tsx
+++ src/components/Page/AboutMe/Contact.tsx
@@ -1,9 +1,25 @@
 'use client';
 
-import Window from '@/components/Window';
+import { motion } from 'framer-motion';
+
 import Link from 'next/link';
 
+import useWindowStore from '@/stores/windowStore';
+
+import Window from '@/components/Window';
+
 export default function Contact() {
+  const { open, restore } = useWindowStore((state) => state);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (!(event.target instanceof HTMLButtonElement)) return;
+
+    open('Email');
+    restore('Email');
+  };
+
+  const MotionLink = motion(Link);
+
   return (
     <Window title="Contact" type="page">
       <div className="flex flex-col items-start gap-[2vh] max-w-[60vh] px-[3.3vh] py-[3vh] mx-auto">
@@ -11,25 +27,29 @@ export default function Contact() {
           Contact Me 😊
         </h3>
         <section className="flex flex-col gap-[2vh] text-black-50 text-[1.5vh] leading-[2.3vh]">
-          <Link
-            href="mailto:nalsae0207@naver.com?subject=안녕하세요."
+          <motion.button
+            onClick={handleClick}
+            type="button"
+            whileHover={{ scale: 1.05 }}
             className="w-fit px-[1vh] py-[0.7vh] border-[0.4vh] border-purple-30 text-[1.8vh] text-purple-70 font-bold shadow-page-dark whitespace-nowrap">
             📫 Email 보내기
-          </Link>
-          <Link
+          </motion.button>
+          <MotionLink
             target="_blank"
             rel="noopener noreferrer"
             href="https://github.com/Nalsae"
+            whileHover={{ scale: 1.05 }}
             className="w-fit px-[1vh] py-[0.7vh] border-[0.4vh] border-purple-30 text-[1.8vh] text-purple-70 font-bold shadow-page-dark whitespace-nowrap">
             📁 GitHub 바로 가기
-          </Link>
-          <Link
+          </MotionLink>
+          <MotionLink
             target="_blank"
             rel="noopener noreferrer"
             href="https://velog.io/@nalsae"
+            whileHover={{ scale: 1.05 }}
             className="w-fit px-[1vh] py-[0.7vh] mb-[1vh] border-[0.4vh] border-purple-30 text-[1.8vh] text-purple-70 font-bold shadow-page-dark whitespace-nowrap">
             📰 Velog 바로 가기
-          </Link>
+          </MotionLink>
         </section>
       </div>
     </Window>
