5f9f6ea01cf0c3d0bb4f5eba76a601b8983ebf26
nalsae <nalsae0207@naver.com>
2023-10-06 04:38:00 +0900

    ✨ Feat : Taskbar 컴포넌트에 추가 기능 구현

--- src/components/Taskbar.tsx
+++ src/components/Taskbar.tsx
@@ -1,69 +1,89 @@
 'use client';
 
-import { v4 as uuid } from 'uuid';
-
-import useWindowStore from '@/stores/windowStore';
-
 import Link from 'next/link';
+
+import { v4 as uuid } from 'uuid';
+import { motion } from 'framer-motion';
+
+import useWindowStore, { Titles } from '@/stores/windowStore';
+
 import Clock from './Clock';
-import React from 'react';
 
 export default function Taskbar() {
-  const { windows, restore } = useWindowStore();
+  const { tasks, open, restore } = useWindowStore((state) => state);
 
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    if (event.target instanceof HTMLButtonElement) {
-      const windowName = event.target.dataset.windowName as string;
+  const handleTask = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (!(event.target instanceof HTMLButtonElement)) return;
 
-      restore(windowName);
-    }
+    const title = event.target.dataset.windowTitle as Titles;
+
+    restore(title);
   };
 
+  const handleEmail = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (!(event.target instanceof HTMLButtonElement)) return;
+
+    open('Email');
+    restore('Email');
+  };
+
+  const MotionLink = motion(Link);
+
   return (
-    <section className="fixed bottom-0 flex items-center w-full h-[8.82vh] border-t-[0.5vh] border-purple-30 bg-purple-10 text-xl font-bold shadow-taskbar z-50">
+    <section
+      id="TASK_BAR"
+      className="fixed bottom-0 flex items-center w-full h-[8.82vh] border-t-[0.5vh] border-purple-30 bg-purple-10 text-xl font-bold shadow-taskbar z-50">
       <div className="w-[9.7vh] h-full px-[1.7vh] py-[1.4vh] border-r-[0.5vh] border-purple-30">
-        <button
+        <motion.button
           type="button"
           title="메뉴"
+          whileHover={{ scale: 1.1 }}
           className="w-full h-full bg-no-repeat bg-contain bg-center bg-[url('/assets/icon/button_menu.png')] drop-shadow-icon-big"
         />
       </div>
       <nav className="ml-[2vh] mr-auto">
         <ul className="flex gap-[1.6vh]">
           <li>
-            <Link
+            <MotionLink
               href="https://github.com/Nalsae"
               target="_blank"
               rel="noopener noreferrer"
               title="GitHub 바로 가기"
+              whileHover={{ scale: 1.1 }}
               className="block w-[5.5vh] h-[5vh] bg-no-repeat bg-contain bg-center bg-[url('/assets/icon/link_github.png')] drop-shadow-icon-big"
             />
           </li>
           <li>
-            <Link
+            <MotionLink
               href="https://velog.io/@nalsae"
               target="_blank"
               rel="noopener noreferrer"
               title="Velog 바로 가기"
+              whileHover={{ scale: 1.1 }}
               className="block w-[5.5vh] h-[5vh] bg-no-repeat bg-contain bg-center bg-[url('/assets/icon/link_velog.png')] drop-shadow-icon-big"
             />
           </li>
-          {windows.map(({ name, url }) => (
+          {tasks.map(({ title, url }) => (
             <li key={uuid()}>
-              <button
-                onClick={handleClick}
+              <motion.button
+                onClick={handleTask}
                 type="button"
                 title="열기"
-                data-window-name={name}
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                whileHover={{ scale: 1.1 }}
+                data-window-title={title}
                 className={`w-[5.5vh] h-[5vh] bg-no-repeat bg-contain bg-center drop-shadow-icon-big ${url}`}
               />
             </li>
           ))}
         </ul>
       </nav>
-      <button
+      <motion.button
+        onClick={handleEmail}
         type="button"
         title="메일 보내기"
+        whileHover={{ scale: 1.1 }}
         className="w-[5.5vh] h-[5vh] mr-[2vh] bg-no-repeat bg-contain bg-center bg-[url('/assets/icon/button_mail.png')] drop-shadow-icon-big"
       />
       <Clock />
