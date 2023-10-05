b8fe11e612f1f789f17d465e6857fea2e986f788
nalsae <nalsae0207@naver.com>
2023-10-06 04:40:25 +0900

    ✨ Feat : Window 컴포넌트에 추가 기능 구현

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -1,16 +1,16 @@
 'use client';
 
-import { animated } from 'react-spring';
 import { twMerge } from 'tailwind-merge';
 
-import useWindowStore from '@/stores/windowStore';
+import useWindowStore, { Titles } from '@/stores/windowStore';
 
 import useResizeWindow from '@/hooks/useResizeWindow';
 import useDragWindow from '@/hooks/useDragWindow';
+import useAnimateWindow from '@/hooks/useAnimateWindow';
 
 interface WindowProps {
   type: 'folder' | 'page';
-  title: string;
+  title: Titles;
   children?: React.ReactNode;
   className?: string;
 }
@@ -21,38 +21,61 @@ export default function Window({
   children,
   className,
 }: WindowProps) {
-  const { minimize, close } = useWindowStore();
+  const { scope } = useAnimateWindow();
 
-  const { divRef, sectionRef } = useResizeWindow();
-  const { position, setPosition } = useDragWindow();
+  const window = useWindowStore((state) => state);
 
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    if (!(event.target instanceof HTMLButtonElement)) return;
+  const { divRef, sectionRef, handleClick, handleDoubleClick } =
+    useResizeWindow(title);
+  const {
+    currentPosition,
+    handleDragStart,
+    handleDrag,
+    handleDragOver,
+    handleDragEnd,
+  } = useDragWindow();
 
-    const title = event.target.closest('ul')?.dataset.title as string;
+  const initialTop = type === 'folder' ? '3.4vh' : '15.4vh';
+  const initialLeft = type === 'folder' ? '14.5vh' : '59vh';
+  const resizeStyle = window[title].isMaximized ? '' : 'resize';
 
-    if (event.target.title === '최소화') minimize(title);
-
-    if (event.target.title === '최대화') {
-    }
-
-    if (event.target.title === '닫기') close(title);
+  const windowStyle = {
+    display: window[title].isMinimized ? 'none' : 'block',
+    left: window[title].isMaximized
+      ? 0
+      : currentPosition.left
+      ? currentPosition.left
+      : initialLeft,
+    top: window[title].isMaximized
+      ? 0
+      : currentPosition.top
+      ? currentPosition.top
+      : initialTop,
+    width: window[title].isMaximized ? '100%' : '50vh',
+    height: window[title].isMaximized ? '100%' : '50vh',
+    zIndex: window[title].isMaximized ? '60' : '0',
+  };
+  const sectionStyle = {
+    width: window[title].isMaximized ? '100%' : '50vh',
+    height: window[title].isMaximized ? '90%' : '28vh',
+    boxShadow: window[title].isMaximized ? 'none' : '4px 4px 4px 0px #6A43B8',
   };
 
-  const windowStyle =
-    type === 'folder'
-      ? 'top-[3.4vh] left-[14.5vh]'
-      : 'top-[12.7vh] right-[14.5vh]';
-
   return (
-    <animated.div
+    <div
+      ref={scope}
+      draggable={!window[title].isMaximized}
+      onDragStart={handleDragStart}
+      onDrag={handleDrag}
+      onDragOver={handleDragOver}
+      onDragEnd={handleDragEnd}
       className={twMerge(
-        `absolute min-w-[27vh] w-[46.45vh] touch-none ${windowStyle}`,
+        `absolute min-w-[27vh] w-[50vh] opacity-0 cursor-grab touch-none`,
         className,
       )}
-      style={{ x: position.x, y: position.y }}>
-      <animated.div
-        {...setPosition()}
+      style={windowStyle}>
+      <div
+        onDoubleClick={handleDoubleClick}
         ref={divRef}
         className="sticky top-0 left-0 flex justify-between items-center w-full min-w-[27vh] pl-[0.8vh] pr-[0.4vh] border-[0.5vh] border-purple-30 bg-purple-10 shadow-window touch-none">
         <h2 className="text-purple-70 text-[1.3vh] font-bold">{title}</h2>
@@ -69,16 +92,17 @@ export default function Window({
             </li>
           ))}
         </ul>
-      </animated.div>
+      </div>
       <section
         ref={sectionRef}
         className={twMerge(
-          'absolute top-[3.05vh] left-0 min-w-[27vh] w-[46.45vh] max-h-[48vh] h-[28vh] border-[0.5vh] border-purple-30 bg-white-10 shadow-window resize overflow-auto scrollbar',
+          `absolute top-[3.05vh] left-0 min-w-[27vh] w-[50vh] max-h-[48vh] h-[28vh] border-[0.5vh] border-purple-30 bg-white-10 shadow-window overflow-auto overflow-x-hidden scrollbar ${resizeStyle}`,
           className,
-        )}>
+        )}
+        style={sectionStyle}>
         {children}
       </section>
-    </animated.div>
+    </div>
   );
 }
 
