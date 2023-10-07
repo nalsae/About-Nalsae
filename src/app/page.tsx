eb6b28a855ab66aaf6c3b9f705e83e937bd2a621
nalsae <nalsae0207@naver.com>
2023-10-07 17:24:17 +0900

    🐛 Fix : Three.js 관련 오류 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
         />
         <Monitor />
         <OrbitControls
-          autoRotate
+          autoRotate={!isClick}
           autoRotateSpeed={5}
           enableRotate={!isClick}
           enableZoom={false}
